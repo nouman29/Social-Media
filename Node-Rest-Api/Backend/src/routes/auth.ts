2f5cb610d185c90170a3352a6535d59512b23ba1
nouman29 <noumanasghar92@gmail.com>
2025-07-05 08:53:41 +0500

    users api

--- Node-Rest-Api/Backend/src/routes/auth.ts
+++ Node-Rest-Api/Backend/src/routes/auth.ts
@@ -1,24 +1,46 @@
 import express from "express";
 import User from "../models/userSchema";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    try {
-    const user=new User({
-        username:req.body.username,
-        email:req.body.email,
-        password:req.body.password,
+  try {
+    // hashed the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // create a new user
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
     });
-        user.save();
-        res.status(201).json(user);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
 
-// router.post("/login", (req, res) => {
-//     res.send("Hello World");
-// });
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+    // login route
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (!user) {
+          return res.status(404).json("User not found");
+        }
+        const validPassword = await bcrypt.compare(
+          req.body.password,
+          user.password
+        );
+        if (!validPassword) {
+          return res.status(400).json("Invalid password");
+        }
+        res.status(200).send("Login successful");
+        } catch (err) {
+        res.status(500).json(err);
+      }
+});
 
 export default router;
