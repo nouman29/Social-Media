7e492b3c4f0e0cb1ce58a753ec037aa3cf546750
nouman29 <noumanasghar92@gmail.com>
2025-07-05 17:42:57 +0500

    basic setup

--- Node-Rest-Api/Frontend/src/App.tsx
+++ Node-Rest-Api/Frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
     <div className='bg-gray-500 h-screen w-screen flex justify-center items-center'>
-     <h1 className='text-3xl font-bold underline'>Hello World</h1>
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
     </div>
     </>
   )
